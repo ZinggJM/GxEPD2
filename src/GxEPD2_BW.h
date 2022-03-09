f67318a489f737843ecf71e51f9ca6d63ca2e75b
Tenz <Martenz@users.noreply.github.com>
2022-03-09 10:04:54 +0100

    trying to include new Lilygo t5 v2.4.1 epaper panel FPC-8101 DEPxxx model with heltec driver code, it works but refresh time seems to be too slow

--- src/GxEPD2_BW.h
+++ src/GxEPD2_BW.h
@@ -14,6 +14,7 @@
 
 // uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
 //#include <GFX.h>
+#define ENABLE_GxEPD2_GFX 1
 
 #ifndef ENABLE_GxEPD2_GFX
 // default is off
@@ -31,8 +32,8 @@
 #endif
 
 #include "GxEPD2_EPD.h"
-#include "epd/GxEPD2_102.h"
 #include "epd/GxEPD2_154.h"
+#include "epd/GxEPD2_154_DEP.h"
 #include "epd/GxEPD2_154_D67.h"
 #include "epd/GxEPD2_154_T8.h"
 #include "epd/GxEPD2_154_M09.h"
@@ -40,33 +41,25 @@
 #include "epd/GxEPD2_213.h"
 #include "epd/GxEPD2_213_B72.h"
 #include "epd/GxEPD2_213_B73.h"
-#include "epd/GxEPD2_213_B74.h"
 #include "epd/GxEPD2_213_flex.h"
 #include "epd/GxEPD2_213_M21.h"
-#include "epd/GxEPD2_213_T5D.h"
 #include "epd/GxEPD2_260.h"
 #include "epd/GxEPD2_260_M01.h"
 #include "epd/GxEPD2_290.h"
 #include "epd/GxEPD2_290_T5.h"
-#include "epd/GxEPD2_290_T5D.h"
-#include "epd/GxEPD2_290_I6FD.h"
 #include "epd/GxEPD2_290_M06.h"
 #include "epd/GxEPD2_290_T94.h"
-#include "epd/GxEPD2_290_T94_V2.h"
 #include "epd/GxEPD2_270.h"
 #include "epd/GxEPD2_371.h"
-#include "epd/GxEPD2_370_TC1.h"
 #include "epd/GxEPD2_420.h"
 #include "epd/GxEPD2_420_M01.h"
 #include "epd/GxEPD2_583.h"
 #include "epd/GxEPD2_583_T8.h"
 #include "epd/GxEPD2_750.h"
 #include "epd/GxEPD2_750_T7.h"
-#include "epd/GxEPD2_1160_T91.h"
 #include "epd/GxEPD2_1248.h"
 #include "it8951/GxEPD2_it60.h"
 #include "it8951/GxEPD2_it60_1448x1072.h"
-#include "it8951/GxEPD2_it78_1872x1404.h"
 
 template<typename GxEPD2_Type, const uint16_t page_height>
 class GxEPD2_BW : public GxEPD2_GFX_BASE_CLASS
@@ -128,12 +121,12 @@ class GxEPD2_BW : public GxEPD2_GFX_BASE_CLASS
       x -= _pw_x;
       y -= _pw_y;
       // clip to (partial) window
-      if ((x < 0) || (x >= int16_t(_pw_w)) || (y < 0) || (y >= int16_t(_pw_h))) return;
+      if ((x < 0) || (x >= _pw_w) || (y < 0) || (y >= _pw_h)) return;
       // adjust for current page
       y -= _current_page * _page_height;
       if (_reverse) y = _page_height - y - 1;
       // check if in current page
-      if ((y < 0) || (y >= int16_t(_page_height))) return;
+      if ((y < 0) || (y >= _page_height)) return;
       uint16_t i = x / 8 + y * (_pw_w / 8);
       if (color)
         _buffer[i] = (_buffer[i] | (1 << (7 - x % 8)));
@@ -153,22 +146,10 @@ class GxEPD2_BW : public GxEPD2_GFX_BASE_CLASS
     // initial false for re-init after processor deep sleep wake up, if display power supply was kept
     // this can be used to avoid the repeated initial full refresh on displays with fast partial update
     // NOTE: garbage will result on fast partial update displays, if initial full update is omitted after power loss
-    // reset_duration = 10 is default; a value of 2 may help with "clever" reset circuit of newer boards from Waveshare 
+    // reset_duration = 20 is default; a value of 2 may help with "clever" reset circuit of newer boards from Waveshare 
     // pulldown_rst_mode true for alternate RST handling to avoid feeding 5V through RST pin
-    void init(uint32_t serial_diag_bitrate, bool initial, uint16_t reset_duration = 10, bool pulldown_rst_mode = false)
+    void init(uint32_t serial_diag_bitrate, bool initial, uint16_t reset_duration = 20, bool pulldown_rst_mode = false)
     {
-      epd2.init(serial_diag_bitrate, initial, reset_duration, pulldown_rst_mode);
-      _using_partial_mode = false;
-      _current_page = 0;
-      setFullWindow();
-    }
-
-    // init method with additional parameters:
-    // SPIClass& spi: either SPI or alternate HW SPI channel
-    // SPISettings spi_settings: e.g. for higher SPI speed selection
-    void init(uint32_t serial_diag_bitrate, bool initial, uint16_t reset_duration, bool pulldown_rst_mode, SPIClass& spi, SPISettings spi_settings)
-    {
-      epd2.selectSPI(spi, spi_settings);
       epd2.init(serial_diag_bitrate, initial, reset_duration, pulldown_rst_mode);
       _using_partial_mode = false;
       _current_page = 0;
@@ -287,7 +268,7 @@ class GxEPD2_BW : public GxEPD2_GFX_BASE_CLASS
       {
         //Serial.print("  nextPage("); Serial.print(_pw_x); Serial.print(", "); Serial.print(_pw_y); Serial.print(", ");
         //Serial.print(_pw_w); Serial.print(", "); Serial.print(_pw_h); Serial.print(") P"); Serial.println(_current_page);
-        uint16_t page_ye = _current_page < int16_t(_pages - 1) ? page_ys + _page_height : HEIGHT;
+        uint16_t page_ye = _current_page < (_pages - 1) ? page_ys + _page_height : HEIGHT;
         uint16_t dest_ys = _pw_y + page_ys; // transposed
         uint16_t dest_ye = gx_uint16_min(_pw_y + _pw_h, _pw_y + page_ye);
         if (dest_ye > dest_ys)
@@ -305,7 +286,7 @@ class GxEPD2_BW : public GxEPD2_GFX_BASE_CLASS
           //Serial.print(dest_ys); Serial.print(".."); Serial.println(dest_ye);
         }
         _current_page++;
-        if (_current_page == int16_t(_pages))
+        if (_current_page == _pages)
         {
           _current_page = 0;
           if (!_second_phase)
@@ -328,7 +309,7 @@ class GxEPD2_BW : public GxEPD2_GFX_BASE_CLASS
         if (!_second_phase) epd2.writeImageForFullRefresh(_buffer, 0, page_ys, WIDTH, gx_uint16_min(_page_height, HEIGHT - page_ys));
         else epd2.writeImageAgain(_buffer, 0, page_ys, WIDTH, gx_uint16_min(_page_height, HEIGHT - page_ys));
         _current_page++;
-        if (_current_page == int16_t(_pages))
+        if (_current_page == _pages)
         {
           _current_page = 0;
           if (epd2.hasFastPartialUpdate)
