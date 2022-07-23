4f3f5317a39ffaacb4ad153ba2e7560f00643acf
reiyawea <yfgao1009@gmail.com>
2022-07-23 19:40:39 +0800

    Add WIDTH_VISIBLE attribute for compatibility to GxEPD2_420c_Z96.h (#1)
    Add WIDTH_VISIBLE attribute for compatibility.

--- src/epd3c/GxEPD2_420c_Z96.h
+++ src/epd3c/GxEPD2_420c_Z96.h
@@ -22,6 +22,7 @@ class GxEPD2_420c_Z96 : public GxEPD2_EPD
     // attributes
     static const uint16_t WIDTH = 400;
     static const uint16_t HEIGHT = 300;
+    static const uint16_t WIDTH_VISIBLE = WIDTH;
     static const GxEPD2::Panel panel = GxEPD2::GDEQ042Z21;
     static const bool hasColor = true;
     static const bool hasPartialUpdate = false;
