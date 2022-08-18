b0d882d51f75eb3301138207253f52ca2e0eb3a6
unknown <dobrishinov@gmail.com>
2022-08-18 14:44:32 +0300

    Fix - error: 'WIDTH_VISIBLE' is not a member of {{GxEPD2_it60 or other from it8951}}

--- src/it8951/GxEPD2_it60.h
+++ src/it8951/GxEPD2_it60.h
@@ -24,6 +24,7 @@ class GxEPD2_it60 : public GxEPD2_EPD
   public:
     // attributes
     static const uint16_t WIDTH = 800;
+    static const uint16_t WIDTH_VISIBLE = WIDTH;
     static const uint16_t HEIGHT = 600;
     static const GxEPD2::Panel panel = GxEPD2::ED060SCT;
     static const bool hasColor = false;
